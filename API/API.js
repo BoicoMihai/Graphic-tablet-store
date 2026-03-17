8020a3db2b193a884045e600ffd157c5fe5b6bd6
boga14 <bogdanprisacaru02@gmail.com>
2026-03-17 08:58:55 +0200

    Finished working weather app

---
+++ API/API.js
@@ -0,0 +1,23 @@
+const apiKey = "70ff21e4d61ede13895323f18465977e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".Humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".Wind").innerHTML = data.wind.speed + " km/h";
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
+checkWeather();
